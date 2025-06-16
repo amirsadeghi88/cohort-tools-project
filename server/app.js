32d3cf6ea4bf639c5014d2a7010d172f2051985d
amirsadeghi <amirsadeghi5991@gmail.com>
2025-06-16 17:22:37 +0200

    basics done

--- server/app.js
+++ server/app.js
@@ -6,8 +6,8 @@ const PORT = 5005;
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
-
+const cohortsData = require("./data/cohorts.json");
+const studentsData = require("./data/students.json");
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
@@ -29,6 +29,13 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res) => {
+res.sendFile(__dirname + "/data/cohorts.json");
+})
+
+app.get("/api/students", (req, res) => {
+res.sendFile(__dirname + "/data/students.json");
+})
 
 // START SERVER
 app.listen(PORT, () => {
