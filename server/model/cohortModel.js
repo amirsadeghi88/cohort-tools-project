d2077944e2b25da66435665d9236441760eab9bf
Rishi G. <mail.rishig@gmail.com>
2025-06-18 17:19:26 +0200

    push

---
+++ server/model/cohortModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CohortSchema = new Schema({
+  inProgress: Boolean,
+  cohortSlug: String,
+  cohortName: String,
+  program: String,
+  campus: String,
+  startDate: Date,
+  endDate: Date,
+  programManager: String,
+  leadTeacher: String,
+  totalHours: Number,
+});
+
+const cohortModel = mongoose.model("cohort", CohortSchema);
+
+module.exports = cohortModel;
